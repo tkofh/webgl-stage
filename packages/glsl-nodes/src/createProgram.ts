d926fb74c98571eb23d0a1aa1afd6daf7e37b655
Tim Morris <timthewebguy@gmail.com>
2023-11-25 11:57:02 -0500

    feat: add debug option

--- packages/glsl-nodes/src/createProgram.ts
+++ packages/glsl-nodes/src/createProgram.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import { collectDependencies } from './helpers'
 import type { Namer } from './namer'
 import { createNamer } from './namer'
@@ -11,6 +12,7 @@ type ProgramSetupResult = {
 type ProgramSetup = (namer: Namer) => ProgramSetupResult
 
 export interface ProgramOptions {
+  debug?: boolean
   precision?: 'lowp' | 'mediump' | 'highp'
 }
 
@@ -18,6 +20,12 @@ export const createProgram = (
   setup: ProgramSetup | ProgramSetupResult,
   options?: ProgramOptions
 ) => {
+  const debug = options?.debug ?? false
+
+  if (debug) {
+    console.log('createProgram', setup, options)
+    console.time('program created')
+  }
   const fragment = createWriter('fragment')
   const vertex = createWriter('vertex')
 
@@ -25,7 +33,19 @@ export const createProgram = (
 
   const result = typeof setup === 'function' ? setup(namer) : setup
 
+  if (debug) {
+    console.log('result', result)
+    console.time('collected unique nodes')
+  }
+
   const uniqueNodes = collectDependencies(result.gl_FragColor, result.gl_Position)
+
+  if (debug) {
+    console.timeEnd('collected unique nodes')
+    console.log('uniqueNodes', uniqueNodes)
+
+    console.time('dependencies resolved')
+  }
 
   const dependencies = new Map<Node, Set<Node>>()
   const dependencyResolutionQueue = Array.from(uniqueNodes).sort(
@@ -51,6 +71,10 @@ export const createProgram = (
     if (didResolve) {
       dependencies.set(current, currentDependencies)
     }
+  }
+
+  if (debug) {
+    console.timeEnd('dependencies resolved')
   }
 
   const isDataNode = (node: Node): node is DataNode<DataType> => 'storage' in node
@@ -100,6 +124,10 @@ export const createProgram = (
   const fragmentNodes = new Set<Node>()
   const vertexNodes = new Set<Node>()
 
+  if (debug) {
+    console.time('fragment nodes collected')
+  }
+
   while (fragmentDiscoveryQueue.length > 0) {
     const current = fragmentDiscoveryQueue.pop()!
     fragmentNodes.add(current)
@@ -111,10 +139,19 @@ export const createProgram = (
     }
   }
 
+  if (debug) {
+    console.timeEnd('fragment nodes collected')
+    console.time('vertex nodes collected')
+  }
+
   while (vertexDiscoveryQueue.length > 0) {
     const current = vertexDiscoveryQueue.pop()!
     vertexNodes.add(current)
     vertexDiscoveryQueue.push(...current.dependencies)
+  }
+
+  if (debug) {
+    console.time('vertex nodes collected')
   }
 
   fragment.addGlobal(`precision ${options?.precision ?? 'mediump'} float;`)
@@ -124,6 +161,10 @@ export const createProgram = (
 
   for (const node of Array.from(vertexNodes).sort(compareNodeOrder)) {
     node.write?.(vertex)
+  }
+
+  if (debug) {
+    console.timeEnd('program created')
   }
 
   return {
--- packages/glsl-nodes/test/nodes.spec.ts
+++ packages/glsl-nodes/test/nodes.spec.ts
@@ -19,191 +19,194 @@ import {
 } from '../src'
 
 describe('nodes', () => {
-  test('it works', ({ expect }) => {
+  test.only('it works', ({ expect }) => {
     expect(2).toBe(2)
 
     const controlPointCount = { x: 3, y: 3 }
 
-    const result = createProgram((namer) => {
-      const controlPointPositions = uniformArray(
-        'vec2',
-        namer.uniform('controlPoints'),
-        controlPointCount.x * controlPointCount.y
-      )
-      const controlPointStartIndex = attribute('float', namer.attribute('controlPointStartIndex'))
-
-      const idenmat = constant('mat4', namer.constant('idenmat'), literal('mat4', ['1.0']))
-
-      const t = attribute('vec2', namer.attribute('t'))
-
-      const tX = swizzle(t, 'x')
-      const tY = swizzle(t, 'y')
-
-      const cpStart = variable(
-        'int',
-        namer.variable('cpStart'),
-        cast(controlPointStartIndex, 'int')
-      )
-
-      const bernX = variable(
-        'vec4',
-        namer.variable('bern_x'),
-        multiply(
-          literal('vec4', [
-            '1.0',
-            tX,
-            pow(tX, literal('float', ['2.0'])),
-            pow(tX, literal('float', ['3.0'])),
-          ]),
-          idenmat
+    const result = createProgram(
+      (namer) => {
+        const controlPointPositions = uniformArray(
+          'vec2',
+          namer.uniform('controlPoints'),
+          controlPointCount.x * controlPointCount.y
         )
-      )
-      const bernY = variable(
-        'vec4',
-        namer.variable('bern_y'),
-        multiply(
-          literal('vec4', [
-            '1.0',
-            tY,
-            pow(tY, literal('float', ['2.0'])),
-            pow(tY, literal('float', ['3.0'])),
-          ]),
-          idenmat
+        const controlPointStartIndex = attribute('float', namer.attribute('controlPointStartIndex'))
+
+        const idenmat = constant('mat4', namer.constant('idenmat'), literal('mat4', ['1.0']))
+
+        const t = attribute('vec2', namer.attribute('t'))
+
+        const tX = swizzle(t, 'x')
+        const tY = swizzle(t, 'y')
+
+        const cpStart = variable(
+          'int',
+          namer.variable('cpStart'),
+          cast(controlPointStartIndex, 'int')
         )
-      )
 
-      const createInterP = (axis: 'x' | 'y') =>
-        variable('vec4', namer.variable(`interP${axis.toUpperCase()}`), [
-          dot(
-            bernX,
+        const bernX = variable(
+          'vec4',
+          namer.variable('bern_x'),
+          multiply(
             literal('vec4', [
-              swizzle(accessArray(controlPointPositions, cpStart), axis),
-              swizzle(
-                accessArray(controlPointPositions, add(cpStart, literal('int', ['1']))),
-                axis
-              ),
-              swizzle(
-                accessArray(controlPointPositions, add(cpStart, literal('int', ['2']))),
-                axis
-              ),
-              swizzle(
-                accessArray(controlPointPositions, add(cpStart, literal('int', ['3']))),
-                axis
-              ),
-            ])
-          ),
-          dot(
-            bernX,
+              '1.0',
+              tX,
+              pow(tX, literal('float', ['2.0'])),
+              pow(tX, literal('float', ['3.0'])),
+            ]),
+            idenmat
+          )
+        )
+        const bernY = variable(
+          'vec4',
+          namer.variable('bern_y'),
+          multiply(
             literal('vec4', [
-              swizzle(
-                accessArray(
-                  controlPointPositions,
-                  add(cpStart, literal('int', [`${controlPointCount.x}`]))
-                ),
-                axis
-              ),
-              swizzle(
-                accessArray(
-                  controlPointPositions,
-                  add(cpStart, literal('int', [`${controlPointCount.x + 1}`]))
-                ),
-                axis
-              ),
-              swizzle(
-                accessArray(
-                  controlPointPositions,
-                  add(cpStart, literal('int', [`${controlPointCount.x + 2}`]))
-                ),
-                axis
-              ),
-              swizzle(
-                accessArray(
-                  controlPointPositions,
-                  add(cpStart, literal('int', [`${controlPointCount.x + 3}`]))
-                ),
-                axis
-              ),
-            ])
-          ),
-          dot(
-            bernX,
-            literal('vec4', [
-              swizzle(
-                accessArray(
-                  controlPointPositions,
-                  add(cpStart, literal('int', [`${controlPointCount.x * 2}`]))
-                ),
-                axis
-              ),
-              swizzle(
-                accessArray(
-                  controlPointPositions,
-                  add(cpStart, literal('int', [`${controlPointCount.x * 2 + 1}`]))
-                ),
-                axis
-              ),
-              swizzle(
-                accessArray(
-                  controlPointPositions,
-                  add(cpStart, literal('int', [`${controlPointCount.x * 2 + 2}`]))
-                ),
-                axis
-              ),
-              swizzle(
-                accessArray(
-                  controlPointPositions,
-                  add(cpStart, literal('int', [`${controlPointCount.x * 2 + 3}`]))
-                ),
-                axis
-              ),
-            ])
-          ),
-          dot(
-            bernX,
-            literal('vec4', [
-              swizzle(
-                accessArray(
-                  controlPointPositions,
-                  add(cpStart, literal('int', [`${controlPointCount.x * 3}`]))
-                ),
-                axis
-              ),
-              swizzle(
-                accessArray(
-                  controlPointPositions,
-                  add(cpStart, literal('int', [`${controlPointCount.x * 3 + 1}`]))
-                ),
-                axis
-              ),
-              swizzle(
-                accessArray(
-                  controlPointPositions,
-                  add(cpStart, literal('int', [`${controlPointCount.x * 3 + 2}`]))
-                ),
-                axis
-              ),
-              swizzle(
-                accessArray(
-                  controlPointPositions,
-                  add(cpStart, literal('int', [`${controlPointCount.x * 3 + 3}`]))
-                ),
-                axis
-              ),
-            ])
-          ),
-        ])
+              '1.0',
+              tY,
+              pow(tY, literal('float', ['2.0'])),
+              pow(tY, literal('float', ['3.0'])),
+            ]),
+            idenmat
+          )
+        )
 
-      const interPX = createInterP('x')
-      const interPY = createInterP('y')
+        const createInterP = (axis: 'x' | 'y') =>
+          variable('vec4', namer.variable(`interP${axis.toUpperCase()}`), [
+            dot(
+              bernX,
+              literal('vec4', [
+                swizzle(accessArray(controlPointPositions, cpStart), axis),
+                swizzle(
+                  accessArray(controlPointPositions, add(cpStart, literal('int', ['1']))),
+                  axis
+                ),
+                swizzle(
+                  accessArray(controlPointPositions, add(cpStart, literal('int', ['2']))),
+                  axis
+                ),
+                swizzle(
+                  accessArray(controlPointPositions, add(cpStart, literal('int', ['3']))),
+                  axis
+                ),
+              ])
+            ),
+            dot(
+              bernX,
+              literal('vec4', [
+                swizzle(
+                  accessArray(
+                    controlPointPositions,
+                    add(cpStart, literal('int', [`${controlPointCount.x}`]))
+                  ),
+                  axis
+                ),
+                swizzle(
+                  accessArray(
+                    controlPointPositions,
+                    add(cpStart, literal('int', [`${controlPointCount.x + 1}`]))
+                  ),
+                  axis
+                ),
+                swizzle(
+                  accessArray(
+                    controlPointPositions,
+                    add(cpStart, literal('int', [`${controlPointCount.x + 2}`]))
+                  ),
+                  axis
+                ),
+                swizzle(
+                  accessArray(
+                    controlPointPositions,
+                    add(cpStart, literal('int', [`${controlPointCount.x + 3}`]))
+                  ),
+                  axis
+                ),
+              ])
+            ),
+            dot(
+              bernX,
+              literal('vec4', [
+                swizzle(
+                  accessArray(
+                    controlPointPositions,
+                    add(cpStart, literal('int', [`${controlPointCount.x * 2}`]))
+                  ),
+                  axis
+                ),
+                swizzle(
+                  accessArray(
+                    controlPointPositions,
+                    add(cpStart, literal('int', [`${controlPointCount.x * 2 + 1}`]))
+                  ),
+                  axis
+                ),
+                swizzle(
+                  accessArray(
+                    controlPointPositions,
+                    add(cpStart, literal('int', [`${controlPointCount.x * 2 + 2}`]))
+                  ),
+                  axis
+                ),
+                swizzle(
+                  accessArray(
+                    controlPointPositions,
+                    add(cpStart, literal('int', [`${controlPointCount.x * 2 + 3}`]))
+                  ),
+                  axis
+                ),
+              ])
+            ),
+            dot(
+              bernX,
+              literal('vec4', [
+                swizzle(
+                  accessArray(
+                    controlPointPositions,
+                    add(cpStart, literal('int', [`${controlPointCount.x * 3}`]))
+                  ),
+                  axis
+                ),
+                swizzle(
+                  accessArray(
+                    controlPointPositions,
+                    add(cpStart, literal('int', [`${controlPointCount.x * 3 + 1}`]))
+                  ),
+                  axis
+                ),
+                swizzle(
+                  accessArray(
+                    controlPointPositions,
+                    add(cpStart, literal('int', [`${controlPointCount.x * 3 + 2}`]))
+                  ),
+                  axis
+                ),
+                swizzle(
+                  accessArray(
+                    controlPointPositions,
+                    add(cpStart, literal('int', [`${controlPointCount.x * 3 + 3}`]))
+                  ),
+                  axis
+                ),
+              ])
+            ),
+          ])
 
-      return {
-        gl_FragColor: output('gl_FragColor', literal('vec4', ['1.0', '1.0', '1.0', '1.0'])),
-        gl_Position: output(
-          'gl_Position',
-          literal('vec4', [dot(bernY, interPX), dot(bernY, interPY), '0.0', '1.0'])
-        ),
-      }
-    })
+        const interPX = createInterP('x')
+        const interPY = createInterP('y')
+
+        return {
+          gl_FragColor: output('gl_FragColor', literal('vec4', ['1.0', '1.0', '1.0', '1.0'])),
+          gl_Position: output(
+            'gl_Position',
+            literal('vec4', [dot(bernY, interPX), dot(bernY, interPY), '0.0', '1.0'])
+          ),
+        }
+      },
+      { debug: true }
+    )
 
     console.log(JSON.stringify(result, null, 2))
   })
