243050c7e8592f064bbcdd9883deba73ea63664d
Tim Morris <timthewebguy@gmail.com>
2023-11-26 12:14:13 -0500

    fix(types): correct return for matrix-vector multiplication

--- packages/glsl-nodes/src/nodes/math.ts
+++ packages/glsl-nodes/src/nodes/math.ts
@@ -27,15 +27,42 @@ export const subtract = <
   write: null,
   expression: `(${x.expression} - ${y.expression})`,
 })
+
+type MultiplicationResult<
+  TLeft extends ArithmeticDataType,
+  TRight extends ArithmeticDataType
+> = TLeft extends 'mat2'
+  ? TRight extends 'vec2'
+    ? 'vec2'
+    : 'mat2'
+  : TLeft extends 'mat3'
+  ? TRight extends 'vec3'
+    ? 'vec3'
+    : 'mat3'
+  : TLeft extends 'mat4'
+  ? TRight extends 'vec4'
+    ? 'vec4'
+    : 'mat4'
+  : TLeft
+
 export const multiply = <
   TValueX extends DataNode<ArithmeticDataType>,
   TValueY extends DataNode<ArithmeticCompatibleTypes[TValueX['type']]>
 >(
   x: TValueX,
   y: TValueY
-): DataNode<TValueX['type'], 'literal' | TValueX['storage'] | TValueY['storage']> => ({
+): DataNode<
+  MultiplicationResult<TValueX['type'], TValueY['type']>,
+  'literal' | TValueX['storage'] | TValueY['storage']
+> => ({
   storage: 'literal',
-  type: x.type,
+  type: (x.type === 'mat2' && y.type === 'vec2'
+    ? 'vec2'
+    : x.type === 'mat3' && y.type === 'vec3'
+    ? 'vec3'
+    : x.type === 'mat4' && y.type === 'vec4'
+    ? 'vec4'
+    : x.type) as MultiplicationResult<TValueX['type'], TValueY['type']>,
   dependencies: [x, y],
   write: null,
   expression: `(${x.expression} * ${y.expression})`,
@@ -47,9 +74,18 @@ export const divide = <
 >(
   x: TValueX,
   y: TValueY
-): DataNode<TValueX['type'], 'literal' | TValueX['storage'] | TValueY['storage']> => ({
+): DataNode<
+  MultiplicationResult<TValueX['type'], TValueY['type']>,
+  'literal' | TValueX['storage'] | TValueY['storage']
+> => ({
   storage: 'literal',
-  type: x.type,
+  type: (x.type === 'mat2' && y.type === 'vec2'
+    ? 'vec2'
+    : x.type === 'mat3' && y.type === 'vec3'
+    ? 'vec3'
+    : x.type === 'mat4' && y.type === 'vec4'
+    ? 'vec4'
+    : x.type) as MultiplicationResult<TValueX['type'], TValueY['type']>,
   dependencies: [x, y],
   write: null,
   expression: `(${x.expression} / ${y.expression})`,
